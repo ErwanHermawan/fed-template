c846ada11ce21cc25c2c450b2b60c578d30f6ef1
ErwanHermawan <erwanhermawan858@gmail.com>
2022-08-10 15:00:24 +0700

    update

--- src/scripts/components/index.js
+++ src/scripts/components/index.js
@@ -3,40 +3,10 @@
 @description: Indexing all components
 --------------------------------------------------------------------------------- */
 
+import WindowResize from './WindowResize';
 import WindowScroll from './WindowScroll';
 import Header from './Header';
-import Example from './Example';
-import WindowResize from './WindowResize';
-import MainBanner from './MainBanner';
-import Services from './Services';
-import Method from './Method';
-import Features from './Features';
-import Testimony from './Testimony';
-import Card from './Card';
-import WidgetArticle from './WidgetArticle';
-import BlogBanner from './BlogBanner';
 import Footer from './Footer';
 import FormElement from './FormElement';
-import ServiceTabs from './ServiceTabs';
-import Registration from './Registration';
-import Plan from './Plan';
 
-export {
-  WindowScroll,
-  Header,
-  Example,
-  WindowResize,
-  MainBanner,
-  Services,
-  Method,
-  Features,
-  Testimony,
-  Card,
-  BlogBanner,
-  Footer,
-  WidgetArticle,
-  FormElement,
-  ServiceTabs,
-  Registration,
-  Plan
-};
+export { WindowScroll, Header, WindowResize, Footer, FormElement };
